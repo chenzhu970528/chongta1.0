1a3924b8b9abdc7925d6319dbc41ee72edbf8266
wmw <1968967200@qq.com>
2018-09-13 16:16:25 +0800

    添加了文件

---
+++ document/user_insert.sql
@@ -0,0 +1,43 @@
+-- 用户表数据user 1:男 0：女
+insert into user (userName,userPwd,userEmail,userPhone,sex,realName) VALUES(
+	'杀马特之光','123456','1234567@qq.com','18261888888',1,'张昊天'
+);
+insert into user (userName,userPwd,userEmail,userPhone,sex,realName) VALUES(
+	'marry','198956','123443567@qq.com','18261888888',0,'吴小莉'
+);
+insert into user (userName,userPwd,userEmail,userPhone,sex,realName) VALUES(
+	'夏天','123456','1234567@qq.com','18261888888',0,'钱多多'
+);
+insert into user (userName,userPwd,userEmail,userPhone,sex,realName) VALUES(
+	'流星雨','123456','1234567@qq.com','18261888888',0,'李尔尔'
+);
+insert into user (userName,userPwd,userEmail,userPhone,sex,realName) VALUES(
+	'天下无双','123456','1234567@qq.com','18261888888',1,'苏展'
+);
+insert into user (userName,userPwd,userEmail,userPhone,sex,realName) VALUES(
+	'jasonDK','123456','1234567@qq.com','18261888888',1,'赵天阔'
+);
+insert into user (userName,userPwd,userEmail,userPhone,sex,realName) VALUES(
+	'小雨淅淅','123456','1234567@qq.com','18261888888',0,'周恬恬'
+);
+insert into user (userName,userPwd,userEmail,userPhone,sex,realName) VALUES(
+	'天道酬请','123456','1234567@qq.com','18261888888',1,'邓起'
+);
+insert into user (userName,userPwd,userEmail,userPhone,sex,realName) VALUES(
+	'好大一口锅','198956','123443567@qq.com','18261888888',1,'吴强强'
+);
+insert into user (userName,userPwd,userEmail,userPhone,sex,realName) VALUES(
+	'青青草原','123456','1234567@qq.com','18261888888',0,'唐翠花'
+);
+insert into user (userName,userPwd,userEmail,userPhone,sex,realName) VALUES(
+	'啦啦啦啦','123456','1234567@qq.com','18261888888',1,'王家豪'
+);
+insert into user (userName,userPwd,userEmail,userPhone,sex,realName) VALUES(
+	'雪花飘飘','123456','1234567@qq.com','18261888888',0,'傅家慧'
+);
+insert into user (userName,userPwd,userEmail,userPhone,sex,realName) VALUES(
+	'candy','123456','1234567@qq.com','18261888888',1,'秦天'
+);
+insert into user (userName,userPwd,userEmail,userPhone,sex,realName) VALUES(
+	'harb','123456','1234567@qq.com','18261888888',1,'周学好'
+);
