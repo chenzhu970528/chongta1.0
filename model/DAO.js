cac34499ffef2aa52b3b1c4b6b30d6343c8ba368
leslie <707296377@qq.com>
2018-09-14 14:09:53 +0800

    9/14/14：09 adoptionsDAO修改

--- model/DAO.js
+++ model/DAO.js
@@ -1,19 +1,21 @@
 //通用数据操作对象
 const  mysql = require('mysql')
-const  config = require('../model/dbconfig')
 //创建数据库连接池
-const pool = mysql.createPool(config)
+const pool = mysql.createPool(require('../model/dbconfig').config)
 //公开的模块方法
 function query(sql,values){
     return new Promise((resolve,reject)=>{
         pool.getConnection(function(err,connection){
             if(err){
+                console.log('数据库连接失败:'+err.message)
                 reject(err)
             }else{
                 connection.query(sql,values,(err,rows)=>{
                     if(err){
+                        console.log('数据库操作失败:'+err.message)
                         reject(err)
                     }else{
+                        console.log('数据库操作成功')
                         resolve(rows);
                     }
                     connection.release();
