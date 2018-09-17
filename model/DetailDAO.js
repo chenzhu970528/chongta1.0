a3c69ed1c514442ec282063a33c8fc6b8bcd86c6
zc <30995730@qq.com>
2018-09-17 21:57:19 +0800

    9.17 18：50分 详情

--- model/DetailDAO.js
+++ model/DetailDAO.js
@@ -4,7 +4,14 @@ const DAO = require('../model/DAO')
 class DB {
     //获取用户详细信息方法
     detailIf(userId) {
-        return DAO('select user.userName,headPic,wechat,userEmail,userPhone,sex,lpPic,lpTime,address from user,lostPets where lostPets.userId=user.userId and user.userId=?', [userId]);
+        return DAO("select user.userName,headPic,wechat,userEmail,userPhone,sex,lpPic,lpTime,address,\n" +
+            "case\n" +
+            "\twhen user.sex=0 then '女' \n" +
+            "\telse '男'\n" +
+            "\t\n" +
+            "end as sex\n" +
+            "from lostpets,user where user.userId=lostpets.userId and user.userId=1", [userId]);
     }
+
 }
 module.exports = new DB();
