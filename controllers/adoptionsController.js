bb1cf7164a95a122b5e2a7413c7da499e8c09857
leslie <707296377@qq.com>
2018-09-18 13:35:14 +0800

    9/18/13：34更新

--- controllers/adoptionsController.js
+++ controllers/adoptionsController.js
@@ -1,18 +1,64 @@
 var adoptionsDAO = require('../model/adoptionsDAO')
+var adoDetailsDAO = require('../model/adoDetailsDAO')
 module.exports = {
+
+    // 获取领养信息
+    getAdoptions: async (ctx,next) =>{
+        let data = await adoptionsDAO.getAdoptions();
+        try{
+            ctx.body = {"code":200,"message":"ok",data:data};
+            return data;
+        }catch (err) {
+            ctx.body = {"code":500,"message":err.toString(),data:[]}
+        }
+    },
+    // 发布领养信息
     addAdoptions:async (ctx,next) => {
         //1.收集数据
-        let adoptions = { };
-        adoptions.userId = '7'
-        adoptions.adoAddress = '北京'
-        adoptions.adoTitle = '可爱萨摩耶求抱走'
-        adoptions.detail = '1岁萨摩耶打过疫苗，不想养了，求好心人领养'
+        let art = {};
+        art.userId = ctx.request.body.userId;
+        art.adoTitle = ctx.request.body.adoTitle;
+        art.detail = ctx.request.body.detail;
+        art.adoType = ctx.request.body.adoType;
+        art.limitTime = ctx.request.body.limitTime;
+        art.birth = ctx.request.body.birth;
+        art.petType = ctx.request.body.petType;
+        art.sex = ctx.request.body.sex;
+        art.adoPic = ctx.request.body.adoPic;
+        art.age = ctx.request.body.age;
         try{
             //2.调用用户数据访问对象的添加方法
-            await adoptionsDAO.addAdoptions(adoptions)
+            await adoptionsDAO.addAdoptions(art)
             //3.反馈结果
-            ctx.body = {"code":200,"message":"ok",data:[]}
+            ctx.body = {"code":200,"message":"ok",data:art}
         }catch(err){
+            ctx.body = {"code":500,"message":err.toString(),data:[]}
+        }
+    },
+    //查看领养宠物详细信息
+    getAdoDetails:async (ctx,next)=>{
+        let jsondata = await adoDetailsDAO.getAdoDetails(ctx.params.adoId)
+        // 获取有意领养者
+        let jsondata2 = await adoDetailsDAO.getAdoDetailsMan(ctx.params.adoId)
+        let data ={}
+        data.jsondata = jsondata[0]
+        data.jsondata2 = jsondata2
+        try{
+            ctx.body = {"code":200,"message":"ok",data:data};
+            return data;
+        }
+        catch (err) {
+            ctx.body = {"code":500,"message":err.toString(),data:[]}
+        }
+    },
+    // 查看领养者详情
+    getAdoUser:async(ctx,next)=>{
+        let data = await adoDetailsDAO.getAdoUser(ctx.params.userId)
+        try{
+            ctx.body = {"code":200,"message":"ok",data:data[0]};
+            return data[0];
+        }
+        catch (err) {
             ctx.body = {"code":500,"message":err.toString(),data:[]}
         }
     }
--- model/adoptionsDAO.js
+++ model/adoptionsDAO.js
@@ -6,7 +6,13 @@ class DB{
     getAdoptions(){
         return DAO('select * from adoptions',[]);
     }
-
+    //发布领养信息
+    addAdoptions(art){
+        return DAO('insert into adoptions ' +
+            '(userId,adoAddress,adoTitle,detail,adoType,limitTime,birth,petType,sex,adoPic,age)' +
+            'values(?,?,?,?,?,?,?,?,?,?,?)',
+            [art.userId,art.adoAddress,art.adoTitle,art.detail,art.adoType,art.limitTime,art.birth,art.petType,art.sex,art.adoPic,art.age])
+    }
 
 }
 module.exports = new DB();
