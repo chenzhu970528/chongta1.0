cdd2acc2716c960b5bb5d08d2a85044dfb0702db
dxh <1845145993@qq.com>
2018-10-20 15:13:32 +0800

    系统消息

--- model/sysmesDAO.js
+++ model/sysmesDAO.js
@@ -4,7 +4,7 @@ const DAO = require('../model/DAO')
 class DB{
     //获取系统消息
     getsysmes(){
-        return DAO('select * from sysmes',[]);
+        return DAO('select * from sysmes order by time desc',[]);
     }
 }
 module.exports = new DB();
