c4332ff4d4e3a95b35bf0e0b3d90d21b5fa647dd
leslie <707296377@qq.com>
2018-10-24 10:08:44 +0800

    注册BUG

--- model/usersDAO.js
+++ model/usersDAO.js
@@ -7,8 +7,8 @@ class USERS{
     }
 //添加用户信息，注册
 addUsers(user){
-    return DAO('insert into user (userName,headPic,signature,userPwd,userEmail,userPhone,sex,wechat,realName,idPic,idNo)values(?,?,?,?,?,?,?,?,?,?,?)',
-        [user.userName,user.headPic,user.signature,user.userPwd,user.userEmail,user.userPhone,user.sex,user.wechat,user.realName,user.idPic,user.idNo,])
+    return DAO('insert into user (userName,headPic,userPwd,userEmail,userPhone,sex,wechat)values(?,?,?,?,?,?,?)',
+        [user.userName,user.headPic,user.userPwd,user.userEmail,user.userPhone,user.sex,user.wechat,])
 }
 //修改用户信息，
 modUsers(user){
