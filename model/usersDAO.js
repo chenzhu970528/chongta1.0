dc1fb5c70cc9adc4c83ed2ffce09e595d0c769cd
leslie <707296377@qq.com>
2018-10-27 15:32:04 +0800

    图片BUG

--- model/usersDAO.js
+++ model/usersDAO.js
@@ -13,9 +13,8 @@ addUsers(user){
 
 //修改用户信息，
 modUsers(user){
-    // console.log(user.userName,user.headPic,user.signature,user.userPwd,user.userEmail,user.userPhone,user.sex,user.wechat,user.realName,user.userId)
-    return DAO('update user set userName=?,headPic=?,signature=?,userPwd=?,userEmail=?,userPhone=?,sex=?,wechat=?,realName=? where userId=?',
-        [user.userName,user.headPic,user.signature,user.userPwd,user.userEmail,user.userPhone,user.sex,user.wechat,user.realName,user.userId])
+    return DAO('update user set userName=?,userEmail=?,sex=?,wechat=?,address=? where userId=?',
+        [user.userName,user.userEmail,user.sex,user.wechat,user.address,user.userId])
 }
 //上传头像
 modUserPic(user){
